1a1559d8df759a41a33eeb47b8e6093c18ca41eb
petermugabo250 <mujohnpeter@gmail.com>
2023-09-06 10:24:30 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+  function validationForm(){
+   let name = document.forms["myForm"]["name"].value;
+     if (name ==""){
+    alert("please fill the empty space");
+    return false;
+
+     }
+
+
+
+  }
